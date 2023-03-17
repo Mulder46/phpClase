42d8a07f8587e51964b36c3f1627ce5085208cd1
Mulder46 <neo.saratoga@gmail.com>
2023-03-17 10:24:41 +0100

    Web versión final

---
+++ ProyectoHTML/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="css/plantilla.css"/>
+    <title>Principal</title>
+</head>
+<body>
+    <!-- Requisitos:
+
+    Home o página de bienvenida 
+    Esta será la página index.html y por lo tanto la que se verá primero. Contendrá el bloque de navegación para poder dirigirnos al resto de las secciones.
+    -----
+    Etiquetas semánticas HTML5
+    En este paso habrás de hacer una revisión de los ficheros html creados y agregar todas las etiquetas semánticas posibles.  
+    CSS3
+    Intenta agregar la mayor cantidad de estilos posibles. -->
+    <?php include ('cabecera.html'); ?>
+    <header>
+        <h1>Principal </h1>
+    </header>
+    
+    
+<div class="definiciones">
+<dl>
+    <dt>
+        Cuanica
+    </dt>
+    <dd>
+        Perteneciente o relativo a los cuantos de energía.
+    </dd>
+    <dt>
+        Cuanto
+    </dt>
+    <dd>
+        Unidad mínima de  energía.
+    </dd>
+    <dt>
+        Superposición
+    </dt>
+    <dd>
+        s un principio fundamental de la mecánica cuántica que sostiene que un sistema físico tal como un electrón, existe en parte en todos sus teóricamente posibles estados (o la configuración de sus propiedades) de forma simultánea, pero, cuando se mide, da un resultado que corresponde a solo una de las posibles configuraciones (como se describe en la interpretación de la mecánica cuántica).
+    </dd>
+    <dt>
+        Principio de incertidumbre
+    </dt>
+    <dd>
+        El principio de incertidumbre o de indeterminación de Heisenberg establece la imposibilidad a nivel subatómico de conocer a un mismo tiempo la posición y el momento o cantidad de movimiento(la velocidad) de una partícula.
+    </dd>
+</dl>
+</div>
+</body>
+</html>
